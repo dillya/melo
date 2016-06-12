e903ccaf255bb2170badf2f5f79f1212b06d0c97
Alexandre Dilly <dillya@sparod.com>
2016-06-12 16:19:58 +0200

    file: add module details with MeloModuleInfo

--- src/melo_file.c
+++ src/melo_file.c
@@ -21,16 +21,31 @@
 
 #include "melo_file.h"
 
+/* Module file info */
+MeloModuleInfo melo_file_info = {
+  .name = "Files",
+  .description = "Navigate and play any of your music files",
+};
+
+static const MeloModuleInfo *melo_file_get_info (MeloModule *module);
+
 G_DEFINE_TYPE (MeloFile, melo_file, MELO_TYPE_MODULE)
 
 static void
 melo_file_class_init (MeloFileClass *klass)
 {
+  MeloModuleClass *mclass = (MeloModuleClass *) klass;
 
+  mclass->get_info = melo_file_get_info;
 }
 
 static void
 melo_file_init (MeloFile *self)
 {
+}
 
+static const MeloModuleInfo *
+melo_file_get_info (MeloModule *module)
+{
+  return &melo_file_info;
 }
