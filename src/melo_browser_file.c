395f9323e11102d7d8cb83ed00a614035b9cbc4d
Alexandre Dilly <dillya@sparod.com>
2016-06-21 00:36:17 +0200

    browser: file: add get_list() implementation
    This patch adds generation of MeloBrowserItem GList with GIO. It
    basically gets the name and type of entry and adds it to the GList.

--- src/melo_browser_file.c
+++ src/melo_browser_file.c
@@ -19,16 +19,79 @@
  * Boston, MA  02110-1301, USA.
  */
 
+#include <gio/gio.h>
+
 #include "melo_browser_file.h"
+
+static GList *melo_browser_file_get_list (MeloBrowser *browser,
+                                          const gchar *path);
 
 G_DEFINE_TYPE (MeloBrowserFile, melo_browser_file, MELO_TYPE_BROWSER)
 
 static void
 melo_browser_file_class_init (MeloBrowserFileClass *klass)
 {
+  MeloBrowserClass *bclass = MELO_BROWSER_CLASS (klass);
+
+  bclass->get_list = melo_browser_file_get_list;
 }
 
 static void
 melo_browser_file_init (MeloBrowserFile *self)
 {
+}
+
+static GList *
+melo_browser_file_get_list (MeloBrowser *browser, const gchar *path)
+{
+  GFileEnumerator *dir_enum;
+  GFileInfo *info;
+  GFile *dir;
+  GList *list = NULL;
+
+  /* Open directory */
+  dir = g_file_new_for_uri (path);
+  if (!dir)
+    return NULL;
+
+  /* Get details */
+  if (g_file_query_file_type (dir, 0, NULL) != G_FILE_TYPE_DIRECTORY) {
+    g_object_unref (dir);
+    return NULL;
+  }
+
+  /* Get list of directory */
+  dir_enum = g_file_enumerate_children (dir, NULL, 0, NULL, NULL);
+  if (!dir_enum) {
+    g_object_unref (dir);
+    return NULL;
+  }
+
+  /* Create list */
+  while ((info = g_file_enumerator_next_file (dir_enum, NULL, NULL))) {
+    MeloBrowserItem *item;
+    const gchar *itype;
+    GFileType type;
+
+    /* Get item type */
+    type = g_file_info_get_file_type (info);
+    if (type == G_FILE_TYPE_REGULAR)
+      itype = "file";
+    else if (type == G_FILE_TYPE_DIRECTORY)
+      itype = "directory";
+    else {
+      g_object_unref (info);
+      continue;
+    }
+
+    /* Create a new browser item and insert in list */
+    item = melo_browser_item_new (g_file_info_get_name (info), itype);
+    list = g_list_insert_sorted (list, item,
+                                 (GCompareFunc) melo_browser_item_cmp);
+    g_object_unref (info);
+  }
+  g_object_unref (dir_enum);
+  g_object_unref (dir);
+
+  return list;
 }
