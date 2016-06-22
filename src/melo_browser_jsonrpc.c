0007ee6ff3072808d2817b479e5152f67b720c74
Alexandre Dilly <dillya@sparod.com>
2016-06-22 23:02:38 +0200

    browser: jsonrpc: add a .get_info method
    This new JSON-RPC method is the same as module.get_info: we get
    browser details with its ID.

--- src/melo_browser_jsonrpc.c
+++ src/melo_browser_jsonrpc.c
@@ -22,6 +22,13 @@
 #include "melo_browser.h"
 #include "melo_jsonrpc.h"
 
+typedef enum {
+  MELO_BROWSER_JSONRPC_FIELDS_NONE = 0,
+  MELO_BROWSER_JSONRPC_FIELDS_NAME = 1,
+  MELO_BROWSER_JSONRPC_FIELDS_DESCRIPTION = 2,
+  MELO_BROWSER_JSONRPC_FIELDS_FULL = 255,
+} MeloBrowserJSONRPCFields;
+
 static MeloBrowser *
 melo_browser_jsonrpc_get_browser (JsonObject *obj, JsonNode **error)
 {
@@ -40,7 +47,99 @@ melo_browser_jsonrpc_get_browser (JsonObject *obj, JsonNode **error)
   return NULL;
 }
 
+static MeloBrowserJSONRPCFields
+melo_browser_jsonrpc_get_fields (JsonObject *obj)
+{
+  MeloBrowserJSONRPCFields fields = MELO_BROWSER_JSONRPC_FIELDS_NONE;
+  const gchar *field;
+  JsonArray *array;
+  guint count, i;
+
+  /* Check if fields is available */
+  if (!json_object_has_member (obj, "fields"))
+    return fields;
+
+  /* Get fields array */
+  array = json_object_get_array_member (obj, "fields");
+  if (!array)
+    return fields;
+
+  /* Parse array */
+  count = json_array_get_length (array);
+  for (i = 0; i < count; i++) {
+    field = json_array_get_string_element (array, i);
+    if (!field)
+      break;
+    if (!g_strcmp0 (field, "none")) {
+      fields = MELO_BROWSER_JSONRPC_FIELDS_NONE;
+      break;
+    } else if (!g_strcmp0 (field, "full")) {
+      fields = MELO_BROWSER_JSONRPC_FIELDS_FULL;
+      break;
+    } else if (!g_strcmp0 (field, "name"))
+      fields |= MELO_BROWSER_JSONRPC_FIELDS_NAME;
+    else if (!g_strcmp0 (field, "description"))
+      fields |= MELO_BROWSER_JSONRPC_FIELDS_DESCRIPTION;
+  }
+
+  return fields;
+}
+
+static JsonObject *
+melo_browser_jsonrpc_info_to_object (const gchar *id,
+                                     const MeloBrowserInfo *info,
+                                     MeloBrowserJSONRPCFields fields)
+{
+  JsonObject *obj = json_object_new ();
+  if (id)
+    json_object_set_string_member (obj, "id", id);
+  if (info) {
+    if (fields & MELO_BROWSER_JSONRPC_FIELDS_NAME)
+      json_object_set_string_member (obj, "name", info->name);
+    if (fields & MELO_BROWSER_JSONRPC_FIELDS_DESCRIPTION)
+      json_object_set_string_member (obj, "description", info->description);
+  }
+  return obj;
+}
+
 /* Method callbacks */
+static void
+melo_browser_jsonrpc_get_info (const gchar *method,
+                               JsonArray *s_params, JsonNode *params,
+                               JsonNode **result, JsonNode **error,
+                               gpointer user_data)
+{
+  MeloBrowserJSONRPCFields fields = MELO_BROWSER_JSONRPC_FIELDS_NONE;
+  const MeloBrowserInfo *info = NULL;
+  MeloBrowser *bro;
+  JsonObject *obj;
+
+  /* Get parameters */
+  obj = melo_jsonrpc_get_object (s_params, params, error);
+  if (!obj)
+    return;
+
+  /* Get browser from ID */
+  bro = melo_browser_jsonrpc_get_browser (obj, error);
+  if (!bro) {
+    json_object_unref (obj);
+    return;
+  }
+
+  /* Get fields */
+  fields = melo_browser_jsonrpc_get_fields (obj);
+  json_object_unref (obj);
+
+  /* Generate list */
+  info = melo_browser_get_info (bro);
+  obj = melo_browser_jsonrpc_info_to_object (NULL, info, fields);
+  g_object_unref (bro);
+
+  /* Return result */
+  *result = json_node_new (JSON_NODE_OBJECT);
+  json_node_take_object (*result, obj);
+}
+
 static void
 melo_browser_jsonrpc_get_list (const gchar *method,
                                JsonArray *s_params, JsonNode *params,
@@ -93,6 +192,19 @@ melo_browser_jsonrpc_get_list (const gchar *method,
 
 /* List of methods */
 static MeloJSONRPCMethod melo_browser_jsonrpc_methods[] = {
+  {
+    .method = "get_info",
+    .params = "["
+              "  {\"name\": \"id\", \"type\": \"string\"},"
+              "  {"
+              "    \"name\": \"fields\", \"type\": \"array\","
+              "    \"required\": false"
+              "  }"
+              "]",
+    .result = "{\"type\":\"object\"}",
+    .callback = melo_browser_jsonrpc_get_info,
+    .user_data = NULL,
+  },
   {
     .method = "get_list",
     .params = "["
