ca7b7fa6ea4f3beb73445b47f4e7b91b1390d443
Alexandre Dilly <dillya@sparod.com>
2016-07-26 23:22:25 +0200

    radio: browser: use Sparod radio website for list
    Sparod radio list website is used to create radio list in get_list()
    and to get stream URL for play() callbacks.
    To get list, we use Libsoup client API.

--- src/modules/radio/melo_browser_radio.c
+++ src/modules/radio/melo_browser_radio.c
@@ -21,6 +21,9 @@
 
 #include <string.h>
 
+#include <libsoup/soup.h>
+#include <json-glib/json-glib.h>
+
 #include "melo_browser_radio.h"
 
 /* Radio browser info */
@@ -39,6 +42,7 @@ static gboolean melo_browser_radio_play (MeloBrowser *browser,
 
 struct _MeloBrowserRadioPrivate {
   GMutex mutex;
+  SoupSession *session;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (MeloBrowserRadio, melo_browser_radio, MELO_TYPE_BROWSER)
@@ -49,6 +53,9 @@ melo_browser_radio_finalize (GObject *gobject)
   MeloBrowserRadio *browser_radio = MELO_BROWSER_RADIO (gobject);
   MeloBrowserRadioPrivate *priv =
                         melo_browser_radio_get_instance_private (browser_radio);
+
+  /* Free Soup session */
+  g_object_unref (priv->session);
 
   /* Clear mutex */
   g_mutex_clear (&priv->mutex);
@@ -81,6 +88,11 @@ melo_browser_radio_init (MeloBrowserRadio *self)
 
   /* Init mutex */
   g_mutex_init (&priv->mutex);
+
+  /* Create a new Soup session */
+  priv->session = soup_session_new_with_options (
+                                SOUP_SESSION_USER_AGENT, "Melo",
+                                NULL);
 }
 
 static const MeloBrowserInfo *
@@ -93,11 +105,156 @@ static GList *
 melo_browser_radio_get_list (MeloBrowser *browser, const gchar *path,
                              gint offset, gint count)
 {
+  MeloBrowserRadio *bradio = MELO_BROWSER_RADIO (browser);
+  SoupMessage *msg;
+  GInputStream *stream;
+  JsonParser *parser;
+  JsonNode *node;
+  JsonArray *array;
+  JsonObject *obj;
+  GList *list = NULL;
+  gchar *url;
+  gint page;
+  gint i;
+
+  /* Generate URL */
+  page = (offset / count) + 1;
+  url = g_strdup_printf ("http://www.sparod.com/radio%s?count=%d&page=%d",
+                         path, count, page);
+
+  /* Create request */
+  msg = soup_message_new ("GET", url);
+  g_free (url);
+
+  /* Send message and wait answer */
+  stream = soup_session_send (bradio->priv->session, msg, NULL, NULL);
+  if (!stream)
+    goto bad_request;
+
+  /* Bad status */
+  if (msg->status_code != 200)
+    goto bad_status;
+
+  /* Parse JSON */
+  parser = json_parser_new ();
+  if (!json_parser_load_from_stream (parser, stream, NULL, NULL))
+    goto bad_json;
+
+  /* Get root node and check its type */
+  node = json_parser_get_root (parser);
+  if (!node || json_node_get_node_type (node) != JSON_NODE_ARRAY)
+    goto bad_json;
+
+  /* Get array from node */
+  array = json_node_get_array (node);
+  count = json_array_get_length (array);
+  for (i = 0; i < count; i ++) {
+    const gchar *name, *full_name, *type;
+    MeloBrowserItem *item;
+
+    /* Get next entry */
+    obj = json_array_get_object_element (array, i);
+    if (!obj)
+      continue;
+
+    /* Get name, full_name and type */
+    name = json_object_get_string_member (obj, "id");
+    full_name = json_object_get_string_member (obj, "name");
+    type = json_object_get_string_member (obj, "type");
+
+    /* Generate new item */
+    item = melo_browser_item_new (name, NULL);
+    item->full_name = full_name ? g_strdup (full_name) : g_strdup ("Unknown");
+    item->type = *type == 'm' ? g_strdup ("category") : g_strdup ("radio");
+
+    /* Add item to list */
+    list = g_list_prepend (list, item);
+  }
+
+  /* Reverse list */
+  list = g_list_reverse (list);
+
+  /* Free objects */
+  g_object_unref (parser);
+  g_object_unref (stream);
+  g_object_unref (msg);
+
+  return list;
+
+bad_json:
+  g_object_unref (parser);
+bad_status:
+  g_object_unref (stream);
+bad_request:
+  g_object_unref (msg);
   return NULL;
 }
 
 static gboolean
 melo_browser_radio_play (MeloBrowser *browser, const gchar *path)
 {
+  MeloBrowserRadio *bradio = MELO_BROWSER_RADIO (browser);
+  SoupMessage *msg;
+  GInputStream *stream;
+  JsonParser *parser;
+  JsonNode *node;
+  JsonObject *obj;
+  const gchar *name, *surl;
+  gchar *url;
+  gboolean ret;
+
+  /* Generate URL */
+  url = g_strdup_printf ("http://www.sparod.com/radio%s", path);
+
+  /* Create request */
+  msg = soup_message_new ("GET", url);
+  g_free (url);
+
+  /* Send message and wait answer */
+  stream = soup_session_send (bradio->priv->session, msg, NULL, NULL);
+  if (!stream)
+    goto bad_request;
+
+  /* Bad status */
+  if (msg->status_code != 200)
+    goto bad_status;
+
+  /* Parse JSON */
+  parser = json_parser_new ();
+  if (!json_parser_load_from_stream (parser, stream, NULL, NULL))
+    goto bad_json;
+
+  /* Get root node and check its type */
+  node = json_parser_get_root (parser);
+  if (!node || json_node_get_node_type (node) != JSON_NODE_OBJECT)
+    goto bad_json;
+
+  /* Get object from node */
+  obj = json_node_get_object (node);
+  if (!obj)
+    goto bad_json;
+
+  /* Get stream URL */
+  name = json_object_get_string_member (obj, "name");
+  surl = json_object_get_string_member (obj, "url");
+  if (!surl)
+    return FALSE;
+
+  /* Play radio */
+  ret = melo_player_play (browser->player, surl, name, NULL, FALSE);
+
+  /* Free objects */
+  g_object_unref (parser);
+  g_object_unref (stream);
+  g_object_unref (msg);
+
+  return ret;
+
+bad_json:
+  g_object_unref (parser);
+bad_status:
+  g_object_unref (stream);
+bad_request:
+  g_object_unref (msg);
   return FALSE;
 }
