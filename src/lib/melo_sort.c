87a5db451dfd01a6be2d9c055a0ee060b2167100
Alexandre Dilly <dillya@sparod.com>
2017-12-29 18:01:19 +0100

    lib: add a media sort enumerator
    This patch adds a new enumerator type named 'MeloSort' to define type
    of sort and order to do on media. In addition, some helpers are added
    to get ascendant / descendant order from MeloSort or to convert
    to/from a string.

---
+++ src/lib/melo_sort.c
@@ -0,0 +1,81 @@
+/*
+ * melo_sort.c: Media sort enums and helpers
+ *
+ * Copyright (C) 2017 Alexandre Dilly <dillya@sparod.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+#include "melo_sort.h"
+
+struct {
+  MeloSort sort;
+  const gchar *name;
+  const gchar *name_desc;
+} static const melo_sort_map[] = {
+  { MELO_SORT_NONE,    "none",    "none"        },
+  { MELO_SORT_SHUFFLE, "shuffle", "shuffle"     },
+  { MELO_SORT_FILE,    "file",    "file_desc"   },
+  { MELO_SORT_TITLE,   "title",   "title_desc"  },
+  { MELO_SORT_ARTIST,  "artist",  "artist_desc" },
+  { MELO_SORT_ALBUM,   "album",   "album_desc"  },
+  { MELO_SORT_GENRE,   "genre",   "genre_desc"  },
+  { MELO_SORT_DATE,    "date",    "date_desc"   },
+  { MELO_SORT_TRACK,   "track",   "track_desc"  },
+  { MELO_SORT_TRACKS,  "tracks",  "tracks_desc" },
+};
+
+const gchar *
+melo_sort_to_string (MeloSort sort)
+{
+  MeloSort s =  melo_sort_set_asc (sort);
+  gint i;
+
+  if (!melo_sort_is_valid (sort))
+    return NULL;
+
+  for (i = 0; i < G_N_ELEMENTS (melo_sort_map); i++)
+    if (s == melo_sort_map[i].sort)
+      return melo_sort_is_desc (sort) ? melo_sort_map[i].name_desc :
+                                        melo_sort_map[i].name;
+
+  return NULL;
+}
+
+MeloSort
+melo_sort_from_string (const gchar *name)
+{
+  gint i;
+
+  for (i = 0; i < G_N_ELEMENTS (melo_sort_map); i++)
+    if (!g_strcmp0 (melo_sort_map[i].name, name) ||
+        !g_strcmp0 (melo_sort_map[i].name_desc, name))
+      return melo_sort_map[i].sort;
+
+  return MELO_SORT_COUNT;
+}
+
+gint
+melo_sort_cmp_none (gconstpointer a, gconstpointer b)
+{
+  return 0;
+}
+
+gint
+melo_sort_cmp_shuffle (gconstpointer a, gconstpointer b)
+{
+  return g_random_int_range (-10, 10);
+}
---
+++ src/lib/melo_sort.h
@@ -0,0 +1,118 @@
+/*
+ * melo_sort.h: Media sort enums and helpers
+ *
+ * Copyright (C) 2017 Alexandre Dilly <dillya@sparod.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+#ifndef __MELO_SORT_H__
+#define __MELO_SORT_H__
+
+#include <glib.h>
+
+typedef enum {
+  MELO_SORT_NONE = 0,
+
+  /* Shuffle medias */
+  MELO_SORT_SHUFFLE,
+
+  /* Sort by file name */
+  MELO_SORT_FILE,
+
+  /* Sort by tags */
+  MELO_SORT_TITLE,
+  MELO_SORT_ARTIST,
+  MELO_SORT_ALBUM,
+  MELO_SORT_GENRE,
+  MELO_SORT_DATE,
+  MELO_SORT_TRACK,
+  MELO_SORT_TRACKS,
+
+  /* Sort count */
+  MELO_SORT_COUNT,
+} MeloSort;
+
+#define MELO_SORT_DESC 0x1000
+#define MELO_SORT_MASK (MELO_SORT_DESC - 1)
+
+static inline gboolean
+melo_sort_is_valid (MeloSort sort)
+{
+  return (sort & MELO_SORT_MASK) < MELO_SORT_COUNT;
+}
+
+static inline MeloSort
+melo_sort_set_asc (MeloSort sort)
+{
+  return (sort & ~MELO_SORT_DESC);
+}
+
+static inline MeloSort
+melo_sort_set_desc (MeloSort sort)
+{
+  return (sort | MELO_SORT_DESC);
+}
+
+static inline MeloSort
+melo_sort_invert (MeloSort sort)
+{
+  return (sort ^ MELO_SORT_DESC);
+}
+
+static inline gboolean
+melo_sort_is_asc (MeloSort sort)
+{
+  return !(sort & MELO_SORT_DESC);
+}
+
+static inline gboolean
+melo_sort_is_desc (MeloSort sort)
+{
+  return (sort & MELO_SORT_DESC);
+}
+
+const gchar *melo_sort_to_string (MeloSort sort);
+MeloSort melo_sort_from_string (const gchar *name);
+
+gint melo_sort_cmp_none (gconstpointer a, gconstpointer b);
+gint melo_sort_cmp_shuffle (gconstpointer a, gconstpointer b);
+
+#define melo_sort_cmp_file(a,b) (g_strcmp0 (a, b))
+#define melo_sort_cmp_file_desc(a,b) (-g_strcmp0 (a, b))
+
+#define melo_sort_cmp_title(a,b) (g_strcmp0 (a, b))
+#define melo_sort_cmp_title_desc(a,b) (-g_strcmp0 (a, b))
+
+#define melo_sort_cmp_artist(a,b) (g_strcmp0 (a, b))
+#define melo_sort_cmp_artist_desc(a,b) (-g_strcmp0 (a, b))
+
+#define melo_sort_cmp_album(a,b) (g_strcmp0 (a, b))
+#define melo_sort_cmp_album_desc(a,b) (-g_strcmp0 (a, b))
+
+#define melo_sort_cmp_genre(a,b) (g_strcmp0 (a, b))
+#define melo_sort_cmp_genre_desc(a,b) (-g_strcmp0 (a, b))
+
+#define melo_sort_cmp_date(a,b) (a > b)
+#define melo_sort_cmp_date_desc(a,b) (a < b)
+
+#define melo_sort_cmp_track(a,b) (a > b)
+#define melo_sort_cmp_track_desc(a,b) (a < b)
+
+#define melo_sort_cmp_tracks(a,b) (a > b)
+#define melo_sort_cmp_tracks_desc(a,b) (a < b)
+
+#endif /* __MELO_SORT_H__ */
