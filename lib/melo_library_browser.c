845fbede00a444b5f3bc2832bfba671e95d78689
Alexandre Dilly <dillya@sparod.com>
2020-10-25 01:42:54 +0200

    lib: library: browser: implement first version
    This patch adds first implementation of the default library browser:
    it supports browsing through artists, albums, genres and songs. In
    addition, a basic search can be done to retrieve songs from library.

--- lib/melo_library_browser.c
+++ lib/melo_library_browser.c
@@ -15,10 +15,23 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
  */
 
+#include "melo/melo_library.h"
+#include "melo/melo_playlist.h"
+
 #define MELO_LOG_TAG "library_browser"
 #include "melo/melo_log.h"
 
+#include "browser.pb-c.h"
+
 #include "melo_library_browser.h"
+
+#define MELO_LIBRARY_MAX_COUNT 1000
+
+typedef struct {
+  Browser__Response__MediaList *media_list;
+  Browser__Response__MediaItem *items;
+  Tags__Tags *tags;
+} MeloLibraryBrowserAsync;
 
 struct _MeloLibraryBrowser {
   GObject parent_instance;
@@ -26,9 +39,16 @@ struct _MeloLibraryBrowser {
 
 MELO_DEFINE_BROWSER (MeloLibraryBrowser, melo_library_browser)
 
+static bool melo_library_browser_handle_request (
+    MeloBrowser *browser, const MeloMessage *msg, MeloRequest *req);
+
 static void
 melo_library_browser_class_init (MeloLibraryBrowserClass *klass)
 {
+  MeloBrowserClass *parent_class = MELO_BROWSER_CLASS (klass);
+
+  /* Setup callbacks */
+  parent_class->handle_request = melo_library_browser_handle_request;
 }
 
 static void
@@ -41,5 +61,460 @@ melo_library_browser_new ()
 {
   return g_object_new (MELO_TYPE_LIBRARY_BROWSER, "id", MELO_LIBRARY_BROWSER_ID,
       "name", "Library", "description", "Navigate through all your medias",
-      "icon", "fa:music", NULL);
+      "icon", "fa:music", "support-search", true, NULL);
+}
+
+static bool
+melo_library_browser_get_root (MeloRequest *req)
+{
+  static const struct {
+    const char *id;
+    const char *name;
+    const char *icon;
+  } root[] = {
+      {"artists", "Artists", "fa:user"},
+      {"albums", "Albums", "fa:compact-disc"},
+      {"songs", "Songs", "fa:music"},
+      {"genres", "Genres", "fa:guitar"},
+  };
+  Browser__Response resp = BROWSER__RESPONSE__INIT;
+  Browser__Response__MediaList media_list = BROWSER__RESPONSE__MEDIA_LIST__INIT;
+  Browser__Response__MediaItem *items_ptr[G_N_ELEMENTS (root)];
+  Browser__Response__MediaItem items[G_N_ELEMENTS (root)];
+  Tags__Tags tags[G_N_ELEMENTS (root)];
+  MeloMessage *msg;
+  unsigned int i;
+
+  /* Set response type */
+  resp.resp_case = BROWSER__RESPONSE__RESP_MEDIA_LIST;
+  resp.media_list = &media_list;
+
+  /* Set item list */
+  media_list.n_items = G_N_ELEMENTS (root);
+  media_list.items = items_ptr;
+
+  /* Set list count and offset */
+  media_list.count = G_N_ELEMENTS (root);
+  media_list.offset = 0;
+
+  /* Add media items */
+  for (i = 0; i < G_N_ELEMENTS (root); i++) {
+    /* Init media item */
+    browser__response__media_item__init (&items[i]);
+    media_list.items[i] = &items[i];
+
+    /* Set media */
+    items[i].id = (char *) root[i].id;
+    items[i].name = (char *) root[i].name;
+    items[i].type = BROWSER__RESPONSE__MEDIA_ITEM__TYPE__FOLDER;
+
+    /* Set tags */
+    tags__tags__init (&tags[i]);
+    items[i].tags = &tags[i];
+    tags[i].cover = (char *) root[i].icon;
+  }
+
+  /* Pack message */
+  msg = melo_message_new (browser__response__get_packed_size (&resp));
+  melo_message_set_size (
+      msg, browser__response__pack (&resp, melo_message_get_data (msg)));
+
+  /* Send media list response */
+  melo_request_send_response (req, msg);
+
+  /* Release request */
+  melo_request_unref (req);
+
+  return true;
+}
+
+static bool
+melo_library_parse_query (const char *query, MeloLibraryType *type,
+    MeloLibraryField *fields, uint64_t *ids)
+{
+  const char *frag[4];
+  size_t len[4];
+  size_t count = 0;
+
+  /* Split query */
+  frag[count] = query;
+  do {
+    query = strchr (query, '/');
+    if (!query) {
+      len[count] = strlen (frag[count]);
+      if (len[count])
+        count++;
+      break;
+    }
+    len[count] = query - frag[count];
+    frag[++count] = ++query;
+  } while (count < 4);
+
+  /* Parse query */
+  if (len[0] == 7 && !strncmp (frag[0], "artists", len[0])) {
+    if (count == 1) {
+      *type = MELO_LIBRARY_TYPE_ARTIST;
+      return true;
+    } else if (count == 2) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_ARTIST_ID;
+      ids[0] = strtoull (frag[1], NULL, 10);
+      return true;
+    } else if (count == 3) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_MEDIA_ID;
+      ids[0] = strtoull (frag[2], NULL, 10);
+      return true;
+    }
+  } else if (len[0] == 6 && !strncmp (frag[0], "albums", len[0])) {
+    if (count == 1) {
+      *type = MELO_LIBRARY_TYPE_ALBUM;
+      return true;
+    } else if (count == 2) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_ALBUM_ID;
+      ids[0] = strtoull (frag[1], NULL, 10);
+      return true;
+    } else if (count == 3) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_MEDIA_ID;
+      ids[0] = strtoull (frag[2], NULL, 10);
+      return true;
+    }
+  } else if (len[0] == 6 && !strncmp (frag[0], "genres", len[0])) {
+    if (count == 1) {
+      *type = MELO_LIBRARY_TYPE_GENRE;
+      return true;
+    } else if (count == 2) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_GENRE_ID;
+      ids[0] = strtoull (frag[1], NULL, 10);
+      return true;
+    } else if (count == 3) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_MEDIA_ID;
+      ids[0] = strtoull (frag[2], NULL, 10);
+      return true;
+    }
+  } else if (len[0] == 5 && !strncmp (frag[0], "songs", len[0])) {
+    if (count == 1) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      return true;
+    } else if (count == 2) {
+      *type = MELO_LIBRARY_TYPE_MEDIA;
+      fields[0] = MELO_LIBRARY_FIELD_MEDIA_ID;
+      ids[0] = strtoull (frag[1], NULL, 10);
+      return true;
+    }
+  }
+
+  return false;
+}
+
+static Browser__Action category_actions[2] = {
+    {
+        .base = PROTOBUF_C_MESSAGE_INIT (&browser__action__descriptor),
+        .type = BROWSER__ACTION__TYPE__PLAY,
+        .name = "Play all",
+        .icon = "fa:play",
+    },
+    {
+        .base = PROTOBUF_C_MESSAGE_INIT (&browser__action__descriptor),
+        .type = BROWSER__ACTION__TYPE__ADD,
+        .name = "Add all to playlist",
+        .icon = "fa:plus",
+    },
+};
+
+static Browser__Action media_actions[2] = {
+    {
+        .base = PROTOBUF_C_MESSAGE_INIT (&browser__action__descriptor),
+        .type = BROWSER__ACTION__TYPE__PLAY,
+        .name = "Play media",
+        .icon = "fa:play",
+    },
+    {
+        .base = PROTOBUF_C_MESSAGE_INIT (&browser__action__descriptor),
+        .type = BROWSER__ACTION__TYPE__ADD,
+        .name = "Add media to playlist",
+        .icon = "fa:plus",
+    },
+};
+
+static Browser__Action *category_actions_ptr[2] = {
+    &category_actions[0],
+    &category_actions[1],
+};
+
+static Browser__Action *media_actions_ptr[2] = {
+    &media_actions[0],
+    &media_actions[1],
+};
+
+static bool
+media_cb (const MeloLibraryData *data, MeloTags *tags, void *user_data)
+{
+  MeloLibraryBrowserAsync *async = user_data;
+  Browser__Response__MediaItem *item;
+
+  /* Set media item */
+  item = &async->items[async->media_list->n_items];
+  browser__response__media_item__init (item);
+  item->type = BROWSER__RESPONSE__MEDIA_ITEM__TYPE__MEDIA;
+  item->id = g_strdup (data->id);
+  if (!data->name)
+    item->name = g_strdup ("Unknown");
+  else
+    item->name = g_strdup (data->name);
+
+  /* Set tags */
+  if (tags) {
+    Tags__Tags *item_tags;
+
+    /* Allocate a new tags and set it */
+    item_tags = malloc (sizeof (*item_tags));
+    if (item_tags) {
+      tags__tags__init (item_tags);
+      item_tags->title = g_strdup (melo_tags_get_title (tags));
+      item_tags->artist = g_strdup (melo_tags_get_artist (tags));
+      item_tags->album = g_strdup (melo_tags_get_album (tags));
+      item_tags->genre = g_strdup (melo_tags_get_genre (tags));
+      item_tags->track = melo_tags_get_track (tags);
+      item_tags->cover = g_strdup (melo_tags_get_cover (tags));
+      item->tags = item_tags;
+    }
+  }
+
+  /* Add actions */
+  item->n_actions = G_N_ELEMENTS (media_actions_ptr);
+  item->actions = media_actions_ptr;
+
+  /* Add item to list */
+  async->media_list->items[async->media_list->n_items] = item;
+  async->media_list->n_items++;
+
+  return true;
+}
+
+static bool
+category_cb (const MeloLibraryData *data, MeloTags *tags, void *user_data)
+{
+  MeloLibraryBrowserAsync *async = user_data;
+  Browser__Response__MediaItem *item;
+
+  /* Set category item */
+  item = &async->items[async->media_list->n_items];
+  browser__response__media_item__init (item);
+  item->type = BROWSER__RESPONSE__MEDIA_ITEM__TYPE__FOLDER;
+  item->id = g_strdup (data->id);
+  if (!data->name)
+    item->name = g_strdup ("Unknown");
+  else
+    item->name = g_strdup (data->name);
+
+  /* Add actions */
+  item->n_actions = G_N_ELEMENTS (category_actions_ptr);
+  item->actions = category_actions_ptr;
+
+  /* Add item to list */
+  async->media_list->items[async->media_list->n_items] = item;
+  async->media_list->n_items++;
+
+  return true;
+}
+
+static bool
+melo_library_browser_get_media_list (MeloLibraryBrowser *browser,
+    Browser__Request__GetMediaList *r, MeloRequest *req)
+{
+  Browser__Response resp = BROWSER__RESPONSE__INIT;
+  Browser__Response__MediaList media_list = BROWSER__RESPONSE__MEDIA_LIST__INIT;
+  MeloLibraryField fields[3] = {0};
+  MeloLibraryBrowserAsync async;
+  MeloLibraryType type;
+  MeloMessage *msg;
+  const char *query = r->query;
+  uint64_t ids[3] = {0};
+  unsigned int i;
+  size_t count;
+  bool search = false;
+
+  /* Root media list */
+  if (!g_strcmp0 (query, "/"))
+    return melo_library_browser_get_root (req);
+
+  /* Perform search */
+  if (g_str_has_prefix (r->query, "search:")) {
+    search = true;
+    query += 7;
+  } else
+    query++;
+
+  /* Parse query */
+  if (!search && !melo_library_parse_query (query, &type, fields, ids))
+    return false;
+
+  /* Limit count */
+  count = r->count < MELO_LIBRARY_MAX_COUNT ? r->count : MELO_LIBRARY_MAX_COUNT;
+
+  /* Set response type */
+  resp.resp_case = BROWSER__RESPONSE__RESP_MEDIA_LIST;
+  resp.media_list = &media_list;
+
+  /* Allocate media item list */
+  media_list.items = malloc (sizeof (*media_list.items) * count);
+  async.items = malloc (sizeof (*async.items) * count);
+  async.media_list = &media_list;
+
+  /* Get data from library */
+  if (search) {
+    melo_library_find (MELO_LIBRARY_TYPE_MEDIA, media_cb, &async,
+        MELO_LIBRARY_SELECT (NAME) | MELO_LIBRARY_SELECT (TITLE) |
+            MELO_LIBRARY_SELECT (ARTIST) | MELO_LIBRARY_SELECT (ALBUM) |
+            MELO_LIBRARY_SELECT (GENRE) | MELO_LIBRARY_SELECT (TRACK) |
+            MELO_LIBRARY_SELECT (COVER),
+        count, r->offset, MELO_LIBRARY_FIELD_NONE, false, true,
+        MELO_LIBRARY_FIELD_MEDIA, query, MELO_LIBRARY_FIELD_NAME, query,
+        MELO_LIBRARY_FIELD_TITLE, query, MELO_LIBRARY_FIELD_ARTIST, query,
+        MELO_LIBRARY_FIELD_ALBUM, query, MELO_LIBRARY_FIELD_LAST);
+  } else if (type == MELO_LIBRARY_TYPE_MEDIA) {
+    melo_library_find (MELO_LIBRARY_TYPE_MEDIA, media_cb, &async,
+        MELO_LIBRARY_SELECT (NAME) | MELO_LIBRARY_SELECT (TITLE) |
+            MELO_LIBRARY_SELECT (ARTIST) | MELO_LIBRARY_SELECT (ALBUM) |
+            MELO_LIBRARY_SELECT (GENRE) | MELO_LIBRARY_SELECT (TRACK) |
+            MELO_LIBRARY_SELECT (COVER),
+        count, r->offset, MELO_LIBRARY_FIELD_NONE, false, false, fields[0],
+        ids[0], fields[1], ids[1], MELO_LIBRARY_FIELD_LAST);
+  } else {
+    melo_library_find (type, category_cb, &async, MELO_LIBRARY_SELECT (NAME),
+        count, r->offset, MELO_LIBRARY_FIELD_NAME, false, false, fields[0],
+        ids[0], MELO_LIBRARY_FIELD_LAST);
+  }
+
+  /* Add actions */
+  if (!search && type == MELO_LIBRARY_TYPE_MEDIA) {
+    media_list.n_actions = G_N_ELEMENTS (category_actions_ptr);
+    media_list.actions = category_actions_ptr;
+  }
+
+  /* Update count and offset */
+  media_list.count = media_list.n_items;
+  media_list.offset = r->offset;
+
+  /* Pack message */
+  msg = melo_message_new (browser__response__get_packed_size (&resp));
+  melo_message_set_size (
+      msg, browser__response__pack (&resp, melo_message_get_data (msg)));
+
+  /* Free items */
+  for (i = 0; i < media_list.n_items; i++) {
+    tags__tags__free_unpacked (media_list.items[i]->tags, NULL);
+    g_free (media_list.items[i]->id);
+    g_free (media_list.items[i]->name);
+  }
+  free (async.items);
+  free (media_list.items);
+
+  /* Send media list response */
+  melo_request_send_response (req, msg);
+
+  /* Release request */
+  melo_request_unref (req);
+
+  return true;
+}
+
+static bool
+action_cb (const MeloLibraryData *data, MeloTags *tags, void *user_data)
+{
+  Browser__Action__Type *action = user_data;
+  char *path;
+
+  path = g_build_filename (data->path, data->media, NULL);
+  if (*action == BROWSER__ACTION__TYPE__PLAY) {
+    melo_playlist_play_media (
+        data->player, path, data->name, melo_tags_ref (tags));
+    *action = BROWSER__ACTION__TYPE__ADD;
+  } else
+    melo_playlist_add_media (
+        data->player, path, data->name, melo_tags_ref (tags));
+  g_free (path);
+
+  return true;
+}
+
+static bool
+melo_library_browser_do_action (MeloLibraryBrowser *browser,
+    Browser__Request__DoAction *r, MeloRequest *req)
+{
+  MeloLibraryType type = MELO_LIBRARY_TYPE_MEDIA;
+  Browser__Action__Type action = r->type;
+  MeloLibraryField fields[3] = {0};
+  const char *path = r->path;
+  uint64_t ids[3] = {0};
+  bool ret = true;
+
+  /* Check action type */
+  if (r->type != BROWSER__ACTION__TYPE__PLAY &&
+      r->type != BROWSER__ACTION__TYPE__ADD)
+    return false;
+
+  /* Action on search item */
+  if (g_str_has_prefix (path, "search:")) {
+    fields[0] = MELO_LIBRARY_FIELD_MEDIA_ID;
+    ids[0] = strtoull (path + 7, NULL, 10);
+  } else if (!melo_library_parse_query (path + 1, &type, fields, ids))
+    return false;
+
+  /* Can play only medias */
+  if (type != MELO_LIBRARY_TYPE_MEDIA)
+    return false;
+
+  /* Get files */
+  melo_library_find (MELO_LIBRARY_TYPE_MEDIA, action_cb, &action,
+      MELO_LIBRARY_SELECT (PLAYER) | MELO_LIBRARY_SELECT (PATH) |
+          MELO_LIBRARY_SELECT (MEDIA) | MELO_LIBRARY_SELECT (NAME) |
+          MELO_LIBRARY_SELECT (TITLE) | MELO_LIBRARY_SELECT (ARTIST) |
+          MELO_LIBRARY_SELECT (ALBUM) | MELO_LIBRARY_SELECT (GENRE) |
+          MELO_LIBRARY_SELECT (TRACK) | MELO_LIBRARY_SELECT (COVER),
+      fields[0] != MELO_LIBRARY_FIELD_MEDIA_ID ? MELO_LIBRARY_MAX_COUNT : 1, 0,
+      MELO_LIBRARY_FIELD_NONE, false, false, fields[0], ids[0],
+      MELO_LIBRARY_FIELD_LAST);
+
+  return ret;
+}
+
+static bool
+melo_library_browser_handle_request (
+    MeloBrowser *browser, const MeloMessage *msg, MeloRequest *req)
+{
+  MeloLibraryBrowser *lbrowser = MELO_LIBRARY_BROWSER (browser);
+  Browser__Request *r;
+  bool ret = false;
+
+  /* Unpack request */
+  r = browser__request__unpack (
+      NULL, melo_message_get_size (msg), melo_message_get_cdata (msg, NULL));
+  if (!r) {
+    MELO_LOGE ("failed to unpack request");
+    return false;
+  }
+
+  /* Handle request */
+  switch (r->req_case) {
+  case BROWSER__REQUEST__REQ_GET_MEDIA_LIST:
+    ret =
+        melo_library_browser_get_media_list (lbrowser, r->get_media_list, req);
+    break;
+  case BROWSER__REQUEST__REQ_DO_ACTION:
+    ret = melo_library_browser_do_action (lbrowser, r->do_action, req);
+    break;
+  default:
+    MELO_LOGE ("request %u not supported", r->req_case);
+  }
+
+  /* Free request */
+  browser__request__free_unpacked (r, NULL);
+
+  return ret;
 }
